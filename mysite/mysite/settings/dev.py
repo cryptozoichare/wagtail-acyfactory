7fd620dd175c076a3a44f9a155926263729e9b69
acyfactory <acypedia@protonmail.com>
2024-05-04 14:37:04 -0700

    init

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)62yk@1&l2(hquq@o$#5*^liiwa6#b8j+v(kutl^1qduh&ju8y"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
