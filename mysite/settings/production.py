15a9e21400ae164efc7107c97a27d8c1230fa97c
acyfactory <acypedia@protonmail.com>
2024-05-06 17:03:57 -0700

    tutorial complete

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,8 +1,18 @@
 from .base import *
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
 
 DEBUG = False
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+ALLOWED_HOSTS = [".acyfactory.com", "acyfactory.nfshost.com"]
+
+SESSION_COOKIE_SECURE = True
+
+SERVER_EMAIL = "root@acyfactory.com"
+DEFAULT_FROM_EMAIL = "webmaster@acyfactory.com"
