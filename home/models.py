01fdb015e590a73c08fe16313ddad9a681a18dbc
acyfactory <acypedia@protonmail.com>
2024-05-09 15:37:09 -0700

    restore home page to default because idk what I did

--- home/models.py
+++ home/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
-from wagtail.admin.panels import FieldPanel
-from base.blocks import BaseStreamBlock
+
 
 class HomePage(Page):
-    body = StreamField(
-        BaseStreamBlock(), verbose_name="Page body", blank=True, use_json_field=True
-    )
-
-    content_panels = Page.content_panels + [
-        FieldPanel('body'),
-    ]
+    pass
