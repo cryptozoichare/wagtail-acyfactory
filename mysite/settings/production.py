6211c69abc0b94412812de2230bb1bc8a03b54f5
acyfactory <acypedia@protonmail.com>
2024-05-12 18:06:57 -0700

    modified:   mysite/settings/production.py

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
-from dotenv import load_dotenv
-load_dotenv()
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
 
 
 DEBUG = False
@@ -12,7 +13,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = [".acyfactory.com", "acyfactory.nfshost.com"]
 
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 SERVER_EMAIL = "root@acyfactory.com"
 DEFAULT_FROM_EMAIL = "webmaster@acyfactory.com"
