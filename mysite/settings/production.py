118a969d4d3581f3e9b86e448c0796149e23c37b
acyfactory <acypedia@protonmail.com>
2024-05-13 00:08:54 -0700

    lessons from deployment

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,24 +1,45 @@
-from .base import *
 import os
-from dotenv import load_dotenv
+import random
+import string
 
-load_dotenv()  # take environment variables from .env.
-
+from .base import *
 
 DEBUG = False
 
-# Raises Django's ImproperlyConfigured
-# exception if SECRET_KEY not in os.environ
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = [".acyfactory.com", "acyfactory.nfshost.com"]
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 SECURE_SSL_REDIRECT = True
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
 
-SERVER_EMAIL = "root@acyfactory.com"
-DEFAULT_FROM_EMAIL = "webmaster@acyfactory.com"
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
+CSRF_TRUSTED_ORIGINS = ["https://acyfactory.com", "https://acyfactory.nfshost.com"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+        },
+    },
+}
+
+WAGTAIL_REDIRECTS_FILE_STORAGE = "cache"
+STATIC_ROOT = "/home/public/static/"
+
+try:
+    from .local import *
+except ImportError:
+    pass
